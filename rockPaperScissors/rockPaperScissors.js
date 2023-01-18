13ff0e06d8b61c027ff66a10b4965a247f3ed38c
tsvetinkv <cnikolova95@gmail.com>
2023-01-18 21:38:16 +0200

    added rock paper scissors game

---
+++ rockPaperScissors/rockPaperScissors.js
@@ -0,0 +1,105 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const playMatch = () => {
+
+      const options = document.querySelectorAll(".options button");
+      const playerHand = document.querySelector(".player-hand");
+      const computerHand = document.querySelector(".computer-hand");
+      const hands = document.querySelectorAll(".hands img");
+  
+      hands.forEach(hand => {
+        hand.addEventListener("animationend", function() {
+          this.style.animation = "";
+        });
+      });
+
+      const computerOptions = ["rock", "paper", "scissors"];
+  
+      options.forEach(option => {
+        option.addEventListener("click", function() {
+
+          const computerNumber = Math.floor(Math.random() * 3);
+          const computerChoice = computerOptions[computerNumber];
+  
+          setTimeout(() => {
+
+            compareHands(this.textContent, computerChoice);
+
+            playerHand.src = `./photos/${this.textContent}.png`;
+            computerHand.src = `./photos/${computerChoice}.png`;
+          }, 2000);
+
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
+        });
+      });
+    };
+  
+    const updateScore = () => {
+      const playerScore = document.querySelector(".player-score p");
+      const computerScore = document.querySelector(".computer-score p");
+      playerScore.textContent = pScore;
+      computerScore.textContent = cScore;
+    };
+  
+    const compareHands = (playerChoice, computerChoice) => {
+
+      const winner = document.querySelector(".winner");
+
+      if (playerChoice === computerChoice) {
+        winner.textContent = "It is a tie";
+        return;
+      }
+
+      if (playerChoice === "rock") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "You Win";
+          pScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
+        }
+      }
+
+      if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "You Win";
+          pScore++;
+          updateScore();
+          return;
+        }
+      }
+
+      if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "You Win";
+          pScore++;
+          updateScore();
+          return;
+        }
+      }
+    };
+  
+    playMatch();
+  };
+  
+
+  game();
+
+  
