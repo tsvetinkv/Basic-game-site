f4ea0ed65a9a62044c86c1984a8730e6c61b56c0
tsvetinkv <cnikolova95@gmail.com>
2023-01-29 13:15:47 +0200

    added memory game

---
+++ memoryGame/memoryGame.js
@@ -0,0 +1,68 @@
+const cards = document.querySelectorAll('.memory-card');
+const movesCount = document.getElementById('numberMoves')
+let count = 0;
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+  decreaseCountOfMoves();
+
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+function decreaseCountOfMoves(){
+    count++;
+    movesCount.innerHTML = count;
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
